6514e64cd2df2b885526f166d059f17f26a25ac3
brooksbenson <brooksbenson01@gmail.com>
2018-09-04 21:54:37 -0600

    Create triangle.js

---
+++ triangle.js
@@ -0,0 +1,17 @@
+// Create a function that displays this shape
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+function triangle() {
+  
+}
+
+// console.log
+// string
+// +
+console.log("#");
